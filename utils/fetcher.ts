3de47ec0f20efbe53601ab991cb1912cc012a693
Medwebdev <mtakisnani09@gmail.com>
2022-10-24 15:59:47 +0100

    Added fetcher

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,6 +1,6 @@
-import axios from 'axios'
+import axios from "./axios"
 
-const fetcher = async <T>(url: string, headers: {}): Promise<T | null> => {
+const fetcher= async <T>(url: string, headers: {}): Promise<T | null> => {
     try {
         const { data } = await axios.get<T>(url, {headers, withCredentials: true })
         return data
