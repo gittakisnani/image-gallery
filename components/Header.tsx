cf646f8e41c90209001ccee128a0d698a6634f52
Medwebdev <mtakisnani09@gmail.com>
2022-11-01 15:06:40 +0100

    Added animation to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ import { AiOutlineMenu } from './Icons'
 import useWindowSize from '../hooks/useWindowSize'
 import { useEffect, useState } from 'react'
 import { User } from '../pages/edit-profile'
-
+import { motion } from 'framer-motion'
 const Header = ({ bg = "bg-transparent", searchBar = true, className, user }: HeaderProps) => {
   const { width } = useWindowSize();
   const [navBar, setNavBar] = useState(true);
@@ -22,7 +22,7 @@ const Header = ({ bg = "bg-transparent", searchBar = true, className, user }: He
   }, [user])
 
   return (
-    <header className={`${bg} ${className}`}>
+    <motion.header initial={{ opacity: .4 }} animate={{ opacity: 1}} className={`${bg} ${className}`}>
         <Container className='flex items-center justify-between p-4 md:px-6'>
           <Logo />
           { searchBar && width! > 440 && <div className='flex-1 px-4'>
@@ -31,7 +31,7 @@ const Header = ({ bg = "bg-transparent", searchBar = true, className, user }: He
           { (width! >= 768 || width! < 768 && navBar) && <NavBar user={userData!} setNavBar={setNavBar} /> }
           { width! < 768 && !navBar && <button className='text-2xl' onClick={() => setNavBar(true)}><AiOutlineMenu /></button>}
         </Container>
-    </header>
+    </motion.header>
   )
 }
 
